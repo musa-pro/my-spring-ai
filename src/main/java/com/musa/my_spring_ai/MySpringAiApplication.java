ee72a9d2fa4ab3b2670a5c54cde8f29188ce4141
Mohammad Abu Musa <MohammadAbu.Musa@thomsonreuters.com>
2025-05-05 17:37:20 -0400

    Initial import

---
+++ src/main/java/com/musa/my_spring_ai/MySpringAiApplication.java
@@ -0,0 +1,13 @@
+package com.musa.my_spring_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musa/my_spring_ai/MySpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musa.my_spring_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
